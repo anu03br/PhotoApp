6534c0534de48c7861ccd2c5986932c4a8bf80b1
anu03br <nueand@gmail.com>
2024-12-07 14:46:04 +0100

    Initial commit

---
+++ backend/src/test/java/com/anu03/jdbcTest/JdbcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anu03.jdbcTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
