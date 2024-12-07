163938dbbcc72a8835c3a1d42b6a21c1b9772c9f
giuliafallati <146712586+giuliafallati@users.noreply.github.com>
2024-12-07 17:49:04 +0100

    all working - 32:05

---
+++ backend/src/main/java/com/anu03/jdbcTest/PhotosController.java
@@ -0,0 +1,55 @@
+package com.anu03.jdbcTest;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collection;
+import java.util.HashMap; // TODO anschauen was HashMap ist
+import java.util.Map;
+import java.util.UUID;
+
+
+//this annotation will tell spring to automatically create a new instance when running main
+@RestController
+public class PhotosController {
+    //    if we goto 'http://localhost:8080/' this should be run
+
+    private Map<String, Photo> db = new HashMap<>() {{
+        put("1", new Photo("1", "hello.jpg"));
+    }};
+
+
+    @GetMapping("/")
+    public String hello() {
+//        here we can return HTML elements as a String
+        return "<h1>Welcome to the project</h1><p>This is a Spring Boot app!</p>";
+    }
+
+    @GetMapping ("/photos")
+    public Collection<Photo> get() {
+        return db.values();
+    }
+
+    @GetMapping ("/photos/{id}")
+    public Photo get(@PathVariable String id) {
+        Photo photo = db.get(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
+    }
+
+    @DeleteMapping("/photos/{id}")
+    public void delete(@PathVariable String id) {
+        Photo photo = db.remove(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/photos")
+    public Photo create(@RequestBody @Valid Photo photo) {
+        photo.setId(UUID.randomUUID().toString());
+        db.put(photo.getId(), photo);
+        return photo;
+    }
+
+}
