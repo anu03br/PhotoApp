de1fa4f411de3ab13575710b2f2ba86deec4862f
anu03br <nueand@gmail.com>
2024-12-07 20:34:20 +0100

    fix: fixed inverted condition in Downloadcontroller test: tested upload get and download functionality

--- backend/src/main/java/com/anu03/jdbcTest/DownloadController.java
+++ backend/src/main/java/com/anu03/jdbcTest/DownloadController.java
@@ -1,32 +1,38 @@
 package com.anu03.jdbcTest;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class DownloadController {
 
     //this part will lead to errors because it creates a circular dependency
     //the DownloadController is instancing itself, this is not allowed for a bean
-//    private final DownloadController downloadController;
-//
-//    public DownloadController() {
-//        this.downloadController = downloadController;
-//    }
-    public DownloadController() {
+    private final PhotosService photosService;
+    ;
 
+    public DownloadController(PhotosService photosService) {
+        this.photosService = photosService;
     }
 
 
     @GetMapping("/download/{id}")
-    public ResponseEntity<byte[]> download(@PathVariable int id) {
-        byte[] data = new byte[0];
+    public ResponseEntity<byte[]> download(@PathVariable String id) {
+        Photo photo = photosService.get(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        byte[] data = photo.getData();
         HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.valueOf(photo.getContentType()));
+        ContentDisposition build = ContentDisposition
+                .builder("attachment")
+                .filename(photo.getFileName())
+                .build();
+        headers.setContentDisposition(build);
+
         return new ResponseEntity<>(data, headers, HttpStatus.OK);
     }
 }
