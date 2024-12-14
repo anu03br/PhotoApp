5a9bdb031894d43837d9d11b10f9ec3dc0264734
anu03br <156076912+anu03br@users.noreply.github.com>
2024-12-14 19:23:21 +0100

    Chore:fixed missing link in footerbar Docs:reworte README.MD

--- frontend/src/components/Footerbar.js
+++ frontend/src/components/Footerbar.js
@@ -2,7 +2,7 @@ export default function Footerbar() {
     return (
         <div className="bg-primary p-6 flex justify-between">
             <span>Images ©2024 Paessler GmbH</span>
-            <span className={"text-blue-500 hover:underline"}><a href="https://blog.paessler.com/9-of-the-stupidest-questions-sysadmins-have-to-endure">Admins favourite questions</a></span>
+            <span className={"text-blue-500 hover:underline"}><a href="https://blog.paessler.com/9-of-the-stupidest-questions-sysadmins-have-to-endure" target="_blank">Admins favourite questions</a></span>
             <span>© Brueggli Informatik 2024</span>
         </div>
     )
